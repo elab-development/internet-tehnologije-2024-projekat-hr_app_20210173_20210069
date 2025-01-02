5d2dbb56bf071db75e020f743c883a0bcf68b230
Tijana Ponjarac <tijana.ponjarac@gmail.com>
2025-01-02 15:11:50 +0100

    Opet greska...

--- hr-app/database/factories/EmployeeProjectFactory.php
+++ hr-app/database/factories/EmployeeProjectFactory.php
@@ -14,7 +14,7 @@ class EmployeeProjectFactory extends Factory
             'user_id' => User::factory(),
             'project_id' => Project::factory(),
             'role' => $this->faker->randomElement(['developer', 'manager', 'tester']),
-            'assigned_at' => $this->faker->dateTime(),
+            'assigned_at' => $this->faker->dateTimeBetween('2024-01-01', '2024-12-31')->format('Y-m-d'), // Datum u 2024. godini
         ];
     }
 }
