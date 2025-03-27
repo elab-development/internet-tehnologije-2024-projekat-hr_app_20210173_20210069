0db867e650d02fd9bb914b45e4eadd10c8cbd6d1
Marijana <marijanaslovakovic0@gmail.com>
2025-03-27 20:28:32 +0100

    Stranica za registraciju i css

--- hr-front/src/App.js
+++ hr-front/src/App.js
@@ -1,32 +1,16 @@
 import React from 'react';
-import './App.css';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import RegisterPage from './components/authentification/RegisterPage';
+import './App.css'; 
+
 
 function App() {
   return (
-    <div className="container">
-      <h1>Dobrodošla u HR Aplikaciju</h1>
-      <h2 className="section-title">Pregled tima i aktivnosti</h2>
-
-      <div className="flex">
-        <div className="card">
-          <h3>Broj zaposlenih</h3>
-          <p>25</p>
-        </div>
-        <div className="card">
-          <h3>Aktivni projekti</h3>
-          <p>6</p>
-        </div>
-        <div className="card">
-          <h3>Zahtevi za odsustvo</h3>
-          <p>3 na čekanju</p>
-        </div>
-      </div>
-
-      <div >
-        <h4>Kreiraj novi zahtev za odsustvo</h4>
-        <button>+ Novi zahtev</button>
-      </div>
-    </div>
+      <Router>
+      <Routes>
+        <Route path="/register" element={<RegisterPage />} />
+      </Routes>
+    </Router>
   );
 }
 
