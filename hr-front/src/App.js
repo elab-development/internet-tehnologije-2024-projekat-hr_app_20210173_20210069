ae4c79de831981d9e1f61a90998f2aa77f9ae30d
Marijana <marijanaslovakovic0@gmail.com>
2025-03-27 18:25:49 +0100

    Struktura foldera, logo i testiranje cssa za aplikaciju

--- hr-front/src/App.js
+++ hr-front/src/App.js
@@ -1,23 +1,31 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <h1>Dobrodošla u HR Aplikaciju</h1>
+      <h2 className="section-title">Pregled tima i aktivnosti</h2>
+
+      <div className="flex">
+        <div className="card">
+          <h3>Broj zaposlenih</h3>
+          <p>25</p>
+        </div>
+        <div className="card">
+          <h3>Aktivni projekti</h3>
+          <p>6</p>
+        </div>
+        <div className="card">
+          <h3>Zahtevi za odsustvo</h3>
+          <p>3 na čekanju</p>
+        </div>
+      </div>
+
+      <div >
+        <h4>Kreiraj novi zahtev za odsustvo</h4>
+        <button>+ Novi zahtev</button>
+      </div>
     </div>
   );
 }
